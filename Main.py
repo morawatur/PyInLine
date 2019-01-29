3e0953646a473b86690bb4e5241d1f3b8d805bea
moraw <krzymoraw@gmail.com>
2019-01-29 17:26:49 +0100

    Added list of new problems to solve

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from numba import cuda
+
 import GUI as gui
 
 cuda.select_device(0)
@@ -6,3 +7,8 @@ dev = cuda.get_current_device()
 print('CUDA device in use: ' + dev.name.decode())
 
 gui.RunInLineWindow()
+
+# TODO list
+# Problemy:
+# 1 - przy powtornym zsuwaniu obrazow program sie wykrzacza
+# 2 - reczne przesuwanie obrazow nie dziala na wyswietlany obraz (buffer)
