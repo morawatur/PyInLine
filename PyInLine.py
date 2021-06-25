0c293f0cac68445740d2e514c3167623fcea0d25
moraw <krzymoraw@gmail.com>
2021-06-25 17:20:09 +0200

    Update TODO list

--- PyInLine.py
+++ PyInLine.py
@@ -1,3 +1,9 @@
+# TODO list
+# 1 - update code from PyHoLo
+# 2 - switch to CPU
+# 3 - unify Python style
+# 4 - save/load current session (i.e. save/load current images and parameters to/from binary files)
+
 from numba import cuda
 
 import GUI as gui
@@ -7,8 +13,3 @@ dev = cuda.get_current_device()
 print('CUDA device in use: ' + dev.name.decode())
 
 gui.RunInLineWindow()
-
-# TODO list
-# 1 - re-alignment doesn't work
-# 2 - save/load current session (i.e. save/load current images and parameters to/from binary files)
-# 3 - unify Python style
